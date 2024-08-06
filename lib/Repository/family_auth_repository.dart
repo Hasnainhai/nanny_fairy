e794fa9c2c787c325f3ea21e64dc96e9ac2e4863
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-08-06 15:26:06 +0500

    working on family portion database

---
+++ lib/Repository/family_auth_repository.dart
@@ -0,0 +1,253 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:nanny_fairy/utils/routes/routes_name.dart';
+import 'package:nanny_fairy/utils/utils.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class AuthRepositoryFamily {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthRepositoryFamily({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+  var databaseReference = FirebaseDatabase.instance.ref();
+
+  Future<void> createAccount({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    // Show loading indicator
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final userRef = databaseReference
+          .child('Family')
+          .child(_firebaseAuth.currentUser!.uid);
+
+      await userRef.set({
+        'email': email,
+        'password': password,
+      });
+      // Remove the loading indicator
+      Navigator.of(context).pop();
+      // Show success message or navigate to another screen
+      Utils.toastMessage('Account created successfully!');
+
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        RoutesName.registerFamily,
+        (route) => false,
+      );
+    } on FirebaseAuthException catch (e) {
+      // Remove the loading indicator
+      Navigator.of(context).pop();
+      // Show error message
+      debugPrint(e.message);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to create account: ${e.message}'),
+        ),
+      );
+    }
+  }
+
+  Future<void> saveDetails({
+    required String firstName,
+    required String lastName,
+    required String address,
+    required String houseNumber,
+    required String postCode,
+    required String phoneNumber,
+    required String dob,
+    required BuildContext context,
+  }) async {
+    // Check if any field is empty
+    if (firstName.isEmpty ||
+        lastName.isEmpty ||
+        address.isEmpty ||
+        houseNumber.isEmpty ||
+        postCode.isEmpty ||
+        phoneNumber.isEmpty ||
+        dob.isEmpty) {
+      // Show a toast message
+      Utils.flushBarErrorMessage("Please fill in all fields", context);
+
+      return;
+    }
+
+    // Show loading indicator
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+
+    try {
+      final userId = _firebaseAuth.currentUser?.uid;
+
+      if (userId == null) {
+        throw Exception('User not authenticated');
+      }
+
+      final userRef = databaseReference.child('Family').child(userId);
+
+      await userRef.update({
+        'firstName': firstName,
+        'lastName': lastName,
+        'address': address,
+        'houseNumber': houseNumber,
+        'postCode': postCode,
+        'phoneNumber': phoneNumber,
+        'dob': dob,
+        "uid": userId,
+      });
+
+      // Remove the loading indicator
+      Navigator.of(context).pop();
+
+      // Show success message
+      Utils.toastMessage('Details saved successfully!');
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        RoutesName.selectPassionFamily,
+        (route) => false,
+      );
+    } catch (e) {
+      // Remove the loading indicator
+      Navigator.of(context).pop();
+      Utils.flushBarErrorMessage("Failed to save details", context);
+      debugPrint(e.toString());
+      // Show error message
+    }
+  }
+
+  Future<void> savePassion(
+      List<String> passionList, BuildContext context) async {
+    // Show loading indicator
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+
+    try {
+      final userId = _firebaseAuth.currentUser?.uid;
+
+      if (userId == null) {
+        throw Exception('User not authenticated');
+      }
+
+      final userRef =
+          databaseReference.child('Providers').child(userId).child('Passions');
+
+      await userRef.set(passionList);
+
+      // Close the loading indicator
+      Navigator.of(context).pop();
+
+      Utils.toastMessage('Passions saved successfully!');
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        RoutesName.availabilityView,
+        (route) => false,
+      );
+    } catch (e) {
+      // Close the loading indicator
+      Navigator.of(context).pop();
+
+      // Handle any errors that occur during save
+      print('Error saving passions: $e');
+      Utils.flushBarErrorMessage('Failed to save passions', context);
+    }
+  }
+
+  saveEducationAndHoursrate(String education, hourRate, BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+    try {
+      final userId = _firebaseAuth.currentUser!.uid;
+      final userRef = databaseReference.child('Providers').child(userId);
+      userRef.update({
+        "education": education,
+        "hoursrate": hourRate,
+      });
+      Navigator.of(context).pop();
+
+      Utils.toastMessage('Education and Hours Rate saved successfully!');
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        RoutesName.selectPreference,
+        (route) => false,
+      );
+    } catch (e) {
+      Navigator.of(context).pop();
+
+      // Handle any errors that occur during save
+      print('Error saving passions: $e');
+      Utils.flushBarErrorMessage(
+          'Failed to save education and hours Rate', context);
+    }
+  }
+
+  saveRefernce(
+      String experince, job, skill, land, phone, BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+    try {
+      final userId = _firebaseAuth.currentUser!.uid;
+
+      databaseReference.child('Providers').child(userId).child('Refernce');
+      Navigator.of(context).pop();
+      Utils.toastMessage('Refenrence saved successfully!');
+
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        RoutesName.uploadId,
+        (route) => false,
+      );
+    } catch (e) {
+      Navigator.of(context).pop();
+
+      // Handle any errors that occur during save
+      print('Error saving refernce: $e');
+      Utils.flushBarErrorMessage('Failed to save Refernce', context);
+    }
+  }
+}
