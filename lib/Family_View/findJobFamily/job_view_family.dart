dc60c4347de61454772d6232462cd0f8e6cede93
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-08-07 16:19:36 +0500

    working on family providers view

--- lib/Family_View/findJobFamily/job_view_family.dart
+++ lib/Family_View/findJobFamily/job_view_family.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:nanny_fairy/Family_View/bookFamily/widgets/book_cart_widget_family.dart';
 import 'package:nanny_fairy/Family_View/homeFamily/widgets/bookCart_home_widget.dart';
 import 'package:nanny_fairy/res/components/searchBar.dart';
 import 'package:nanny_fairy/res/components/widgets/vertical_spacing.dart';
 import 'package:nanny_fairy/utils/routes/routes_name.dart';
+import 'package:provider/provider.dart';
+import '../../FamilyController/family_home_controller.dart';
 import '../../res/components/colors.dart';
 
 class JobViewFamily extends StatefulWidget {
@@ -17,6 +18,7 @@ class JobViewFamily extends StatefulWidget {
 class _JobViewFamilyState extends State<JobViewFamily> {
   @override
   Widget build(BuildContext context) {
+    final familyhomeController = Provider.of<FamilyHomeController>(context);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: AppColor.secondaryBgColor,
@@ -71,53 +73,119 @@ class _JobViewFamilyState extends State<JobViewFamily> {
             const VerticalSpeacing(50),
             SizedBox(
               height: MediaQuery.of(context).size.height,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                  child: Column(
-                    children: [
-                      // BookingCartWidgetHome(
-                      //     primaryButtonColor: AppColor.primaryColor,
-                      //     ontapView: () {
-                      //       Navigator.pushNamed(
-                      //           context, RoutesName.providerDetails);
-                      //     },
-                      //     primaryButtonTxt: 'View'),
-                      // BookingCartWidgetHome(
-                      //     primaryButtonColor: AppColor.primaryColor,
-                      //     ontapView: () {
-                      //       Navigator.pushNamed(
-                      //           context, RoutesName.providerDetails);
-                      //     },
-                      //     primaryButtonTxt: 'View'),
-                      // BookingCartWidgetHome(
-                      //     primaryButtonColor: AppColor.primaryColor,
-                      //     ontapView: () {
-                      //       Navigator.pushNamed(
-                      //           context, RoutesName.providerDetails);
-                      //     },
-                      //     primaryButtonTxt: 'View'),
-                      // BookingCartWidgetHome(
-                      //     primaryButtonColor: AppColor.primaryColor,
-                      //     ontapView: () {
-                      //       Navigator.pushNamed(
-                      //           context, RoutesName.providerDetails);
-                      //     },
-                      //     primaryButtonTxt: 'View'),
-                      // BookingCartWidgetHome(
-                      //     primaryButtonColor: AppColor.primaryColor,
-                      //     ontapView: () {
-                      //       Navigator.pushNamed(
-                      //           context, RoutesName.providerDetails);
-                      //     },
-                      //     primaryButtonTxt: 'View'),
-                    ],
-                  ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16),
+                child: FutureBuilder(
+                  future: familyhomeController.getPopularJobs(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    } else if (snapshot.hasData) {
+                      Map<dynamic, dynamic> bookings =
+                          snapshot.data as Map<dynamic, dynamic>;
+                      List<Widget> bookingWidgets = [];
+
+                      bookings.forEach((key, value) {
+                        if (value['Availability'] is Map) {
+                          Map<String, dynamic> availabilityMap =
+                              Map<String, dynamic>.from(value['Availability']);
+                          Set<String> daysSet = {};
+                          availabilityMap.forEach((timeOfDay, daysMap) {
+                            if (daysMap is Map) {
+                              daysMap.forEach((day, isAvailable) {
+                                if (isAvailable && !daysSet.contains(day)) {
+                                  daysSet
+                                      .add(day.substring(0, 1).toUpperCase());
+                                }
+                              });
+                            }
+                          });
+
+                          List<Widget> dayButtons =
+                              daysSet.map((dayAbbreviation) {
+                            return Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: DayButtonFamily(day: dayAbbreviation),
+                            );
+                          }).toList();
+
+                          bookingWidgets.add(
+                            BookingCartWidgetHome(
+                              primaryButtonColor: AppColor.primaryColor,
+                              primaryButtonTxt: 'View',
+                              ontapView: () {
+                                Navigator.pushNamed(
+                                    context, RoutesName.providerDetails);
+                              },
+                              profile: value['profile'],
+                              name:
+                                  "${value['firstName']} ${value['lastName']}",
+                              degree: value['education'],
+                              skill: '',
+                              hoursRate: value['hoursrate'],
+                              dayButtons: dayButtons,
+                            ),
+                          );
+                        } else {
+                          bookingWidgets.add(
+                            const Center(child: Text('Invalid data format')),
+                          );
+                        }
+                      });
+
+                      return SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Column(
+                          children: bookingWidgets,
+                        ),
+                      );
+                    } else {
+                      return const Center(child: Text('No data available'));
+                    }
+                  },
                 ),
               ),
-            )
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class DayButtonFamily extends StatefulWidget {
+  final String day;
+
+  const DayButtonFamily({
+    super.key,
+    required this.day,
+  });
+
+  @override
+  _DayButtonFamilyState createState() => _DayButtonFamilyState();
+}
+
+class _DayButtonFamilyState extends State<DayButtonFamily> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 15,
+      width: 15,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(2),
+        color: Colors.transparent,
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Center(
+        child: Text(
+          widget.day,
+          style: const TextStyle(
+            fontSize: 8,
+            color: AppColor.blackColor,
+            fontWeight: FontWeight.w500,
+          ),
         ),
       ),
     );
