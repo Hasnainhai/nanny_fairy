668069c4802d51ade11097f6a059cf64858fbddd
Basit ali <basitalyshah51214@gmail.com>
2024-08-07 23:39:34 -0700

    changes to make section for the home view

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:nanny_fairy/ViewModel/provider_home_view_model.dart';
 import 'package:nanny_fairy/res/components/colors.dart';
 import 'package:nanny_fairy/res/components/searchBar.dart';
 import 'package:nanny_fairy/res/components/widgets/vertical_spacing.dart';
 import 'package:nanny_fairy/utils/routes/routes_name.dart';
-import 'package:nanny_fairy/view/home/widgets/home_feature_widget.dart';
-import 'package:nanny_fairy/view/job/family_detail_provider.dart';
-import 'package:provider/provider.dart';
-import '../booked/widgets/booking_widget.dart';
+import 'package:nanny_fairy/view/home/widgets/home_default_view.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -20,8 +16,6 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    final homeViewModel = Provider.of<ProviderHomeViewModel>(context);
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(
@@ -84,174 +78,7 @@ class _HomeViewState extends State<HomeView> {
               ],
             ),
             const VerticalSpeacing(50.0),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'This month',
-                    style: GoogleFonts.getFont(
-                      "Poppins",
-                      textStyle: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: AppColor.blackColor,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'All reports',
-                    style: GoogleFonts.getFont(
-                      "Poppins",
-                      textStyle: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: AppColor.primaryColor,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const VerticalSpeacing(10),
-            const SizedBox(
-              height: 140,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(width: 16),
-                    HomeFeatureContainer(
-                      txColor: AppColor.blackColor,
-                      img: 'images/families.png',
-                      title: '100k',
-                      subTitle: 'Total Families',
-                      bgColor: AppColor.whiteColor,
-                    ),
-                    SizedBox(width: 16),
-                    HomeFeatureContainer(
-                      txColor: AppColor.blackColor,
-                      bgColor: AppColor.whiteColor,
-                      img: 'images/chats.png',
-                      title: '10',
-                      subTitle: 'Total Chats',
-                    ),
-                    SizedBox(width: 16),
-                    HomeFeatureContainer(
-                      txColor: AppColor.blackColor,
-                      bgColor: AppColor.whiteColor,
-                      img: 'images/families.png',
-                      title: '100k',
-                      subTitle: 'Total Families',
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const VerticalSpeacing(16.0),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'popular jobs',
-                        style: GoogleFonts.getFont(
-                          "Poppins",
-                          textStyle: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: AppColor.blackColor,
-                          ),
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.pushNamed(context, RoutesName.jobView);
-                        },
-                        child: Text(
-                          'see all',
-                          style: GoogleFonts.getFont(
-                            "Poppins",
-                            textStyle: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              color: AppColor.primaryColor,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const VerticalSpeacing(16.0),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.3,
-                    child: FutureBuilder(
-                      future: homeViewModel.getPopularJobs(),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else if (snapshot.hasError) {
-                          return Center(
-                              child: Text('Error: ${snapshot.error}'));
-                        } else if (snapshot.hasData) {
-                          Map<dynamic, dynamic> bookings =
-                              snapshot.data as Map<dynamic, dynamic>;
-                          List<Widget> bookingWidgets = [];
-
-                          bookings.forEach(
-                            (key, value) {
-                              List<String> passions =
-                                  (value['FamilyPassions'] as List<dynamic>)
-                                      .cast<String>();
-
-                              bookingWidgets.add(
-                                BookingCartWidget(
-                                  primaryButtonTxt: 'View',
-                                  ontapView: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (c) => FamilyDetailProvider(
-                                          name:
-                                              " ${value['firstName']} ${value['lastName']}",
-                                          bio: value['bio'] ?? '',
-                                          profile: value['profile'],
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                  name:
-                                      "${value['firstName']} ${value['lastName']}",
-                                  profilePic: value['profile'],
-                                  passion: passions,
-                                ),
-                              );
-                            },
-                          );
-
-                          return SingleChildScrollView(
-                            scrollDirection: Axis.vertical,
-                            child: Column(
-                              children: bookingWidgets,
-                            ),
-                          );
-                        } else {
-                          return const Center(child: Text('No data available'));
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            const HomeDefaultView(),
           ],
         ),
       ),
---
+++ lib/view/home/widgets/home_search_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeSearchView extends StatefulWidget {
+  const HomeSearchView({super.key});
+
+  @override
+  State<HomeSearchView> createState() => _HomeSearchViewState();
+}
+
+class _HomeSearchViewState extends State<HomeSearchView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
