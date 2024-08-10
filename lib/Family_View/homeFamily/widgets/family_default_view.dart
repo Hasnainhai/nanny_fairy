77988a5650c1a3c104f7875f0bfbbe352a306d9c
Basit ali <basitalyshah51214@gmail.com>
2024-08-10 03:49:21 -0700

    make ui of he deafult and search section

---
+++ lib/Family_View/homeFamily/widgets/family_default_view.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nanny_fairy/FamilyController/family_home_controller.dart';
+import 'package:nanny_fairy/Family_View/findJobFamily/provider_detail.dart';
+import 'package:nanny_fairy/Family_View/homeFamily/home_view_family.dart';
+import 'package:nanny_fairy/Family_View/homeFamily/widgets/bookCart_home_widget.dart';
+import 'package:nanny_fairy/Family_View/homeFamily/widgets/home_Family_feature_widget.dart';
+import 'package:nanny_fairy/res/components/colors.dart';
+import 'package:nanny_fairy/res/components/widgets/vertical_spacing.dart';
+import 'package:nanny_fairy/utils/routes/routes_name.dart';
+import 'package:provider/provider.dart';
+
+class FamilyDefaultView extends StatefulWidget {
+  const FamilyDefaultView({super.key});
+
+  @override
+  State<FamilyDefaultView> createState() => _FamilyDefaultViewState();
+}
+
+class _FamilyDefaultViewState extends State<FamilyDefaultView> {
+  @override
+  Widget build(BuildContext context) {
+    final familyhomeController = Provider.of<FamilyHomeController>(context);
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'What are you looking for',
+                style: GoogleFonts.getFont(
+                  "Poppins",
+                  textStyle: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: AppColor.blackColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const VerticalSpeacing(10),
+        const SizedBox(
+          height: 120,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(width: 16),
+                HomeFeatureContainerFamily(
+                  bgColor: Color(0xff51A1BA),
+                  img: 'images/cleaning.png',
+                  title: 'Cleaning',
+                  txColor: AppColor.whiteColor,
+                ),
+                SizedBox(width: 16),
+                HomeFeatureContainerFamily(
+                  txColor: AppColor.whiteColor,
+                  bgColor: Color(0xffFEAA48),
+                  img: 'images/homeSitter.png',
+                  title: 'Sitter',
+                ),
+                SizedBox(width: 16),
+                HomeFeatureContainerFamily(
+                  txColor: AppColor.whiteColor,
+                  bgColor: Color(0xffDDC912),
+                  img: 'images/cleaning.png',
+                  title: 'Eiderly',
+                ),
+                SizedBox(width: 16),
+                HomeFeatureContainerFamily(
+                  txColor: AppColor.whiteColor,
+                  bgColor: Color(0xffFEAA48),
+                  img: 'images/homeSitter.png',
+                  title: 'Sitter',
+                ),
+                SizedBox(width: 16),
+                HomeFeatureContainerFamily(
+                  txColor: AppColor.whiteColor,
+                  bgColor: Color(0xffDDC912),
+                  img: 'images/cleaning.png',
+                  title: 'Eiderly',
+                ),
+              ],
+            ),
+          ),
+        ),
+        const VerticalSpeacing(16.0),
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'What are you looking for',
+                    style: GoogleFonts.getFont(
+                      "Poppins",
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: AppColor.blackColor,
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, RoutesName.jobViewFamily);
+                    },
+                    child: Text(
+                      'see all',
+                      style: GoogleFonts.getFont(
+                        "Poppins",
+                        textStyle: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: AppColor.primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const VerticalSpeacing(16.0),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.4,
+                child: FutureBuilder(
+                  future: familyhomeController.getPopularJobs(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    } else if (snapshot.hasData) {
+                      Map<dynamic, dynamic> bookings =
+                          snapshot.data as Map<dynamic, dynamic>;
+                      List<Widget> bookingWidgets = [];
+
+                      bookings.forEach((key, value) {
+                        if (value['Availability'] is Map) {
+                          Map<String, dynamic> availabilityMap =
+                              Map<String, dynamic>.from(value['Availability']);
+                          Set<String> daysSet = {};
+
+                          availabilityMap.forEach((timeOfDay, daysMap) {
+                            if (daysMap is Map) {
+                              daysMap.forEach((day, isAvailable) {
+                                if (isAvailable && !daysSet.contains(day)) {
+                                  daysSet
+                                      .add(day.substring(0, 1).toUpperCase());
+                                }
+                              });
+                            }
+                          });
+
+                          List<Widget> dayButtons =
+                              daysSet.map((dayAbbreviation) {
+                            return Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: DayButtonFamily(day: dayAbbreviation),
+                            );
+                          }).toList();
+
+                          bookingWidgets.add(
+                            BookingCartWidgetHome(
+                              primaryButtonColor: AppColor.primaryColor,
+                              primaryButtonTxt: 'View',
+                              ontapView: () {
+                                Map<String, String> timeData =
+                                    (value['Time'] as Map<dynamic, dynamic>)
+                                        .map((key, value) => MapEntry(
+                                            key.toString(), value.toString()));
+
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (c) => ProviderDetails(
+                                            profile: value['profile'],
+                                            name:
+                                                "${value['firstName']} ${value['lastName']}",
+                                            bio: value['bio'],
+                                            horseRate: value['hoursrate'],
+                                            experience: value['Refernce']
+                                                ['experince'],
+                                            degree: value['education'],
+                                            dayButtons: dayButtons,
+                                            timeData: timeData,
+                                          )),
+                                );
+                              },
+                              profile: value['profile'],
+                              name:
+                                  "${value['firstName']} ${value['lastName']}",
+                              degree: value['education'],
+                              skill: '',
+                              hoursRate: value['hoursrate'],
+                              dayButtons: dayButtons,
+                            ),
+                          );
+                        } else {
+                          bookingWidgets.add(
+                            const Center(child: Text('Invalid data format')),
+                          );
+                        }
+                      });
+
+                      return SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 50),
+                          child: Column(
+                            children: bookingWidgets,
+                          ),
+                        ),
+                      );
+                    } else {
+                      return const Center(child: Text('No data available'));
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/Family_View/homeFamily/widgets/family_filter_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FamilyFilterView extends StatefulWidget {
+  const FamilyFilterView({super.key});
+
+  @override
+  State<FamilyFilterView> createState() => _FamilyFilterViewState();
+}
+
+class _FamilyFilterViewState extends State<FamilyFilterView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
