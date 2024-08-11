0e49ccb9dd6a74947d488539a1dbdc6ae3a169e6
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-08-11 18:58:02 +0500

    make some changes

--- lib/main.dart
+++ lib/main.dart
@@ -52,8 +52,8 @@ class MyApp extends StatelessWidget {
           create: (_) => SearchRepository(),
         ),
         ChangeNotifierProvider<FilteredRepository>(
-          create: (context) => FilteredRepository(
-              context.read<SearchRepository>()),
+          create: (context) =>
+              FilteredRepository(context.read<SearchRepository>()),
         ),
         ChangeNotifierProvider<AuthViewModel>(
           create: (context) => AuthViewModel(context.read<AuthRepository>()),
@@ -92,9 +92,8 @@ class MyApp extends StatelessWidget {
           create: (_) => CommunityRepoProvider(),
         ),
         ChangeNotifierProvider<CommunityViewViewModel>(
-          create: (context) =>
-              CommunityViewViewModel(context.read<CommunityRepoProvider>()),
-        ),
+            create: (context) =>
+                CommunityViewViewModel(context.read<CommunityRepoProvider>())),
       ],
       child: MaterialApp(
         initialRoute: RoutesName.splash,
