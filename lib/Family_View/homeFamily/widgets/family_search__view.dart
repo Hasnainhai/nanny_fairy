4a34a3d3ddb5c197f1995c0aa0919d142a003ecf
Basit ali <basitalyshah51214@gmail.com>
2024-08-14 06:28:06 -0700

    changes to fix the issues

--- lib/Family_View/homeFamily/widgets/family_search__view.dart
+++ lib/Family_View/homeFamily/widgets/family_search__view.dart
@@ -75,47 +75,38 @@ class _FamilySearchViewState extends State<FamilySearchView> {
                     itemCount: viewModel.users.length,
                     itemBuilder: (context, index) {
                       final user = viewModel.users[index];
-                      // Assuming `time` can be a Map or another type, handle it accordingly
-                      Map<String, String> timeData = {};
-                      if (user.time is Map) {
-                        timeData = (user.time as Map<dynamic, dynamic>).map(
-                            (key, value) =>
-                                MapEntry(key.toString(), value.toString()));
-                      } else {
-                        // Handle unexpected type for `time`
-                        debugPrint(
-                            'Unexpected type for time: ${user.time.runtimeType}');
-                        // You can provide default values or handle the error appropriately here
-                      }
+                      // List<String> passions = user.passions;
+                      Set<String> daysSet = {};
 
-                      // Set of day buttons based on `user` information
-                      Set<String> daysSet =
-                          {}; // Populate this set based on your data
                       List<Widget> dayButtons = daysSet.map((dayAbbreviation) {
                         return Padding(
                           padding: const EdgeInsets.all(4.0),
                           child: DayButtonFamily(day: dayAbbreviation),
                         );
                       }).toList();
-
                       return BookingCartWidgetHome(
                         primaryButtonColor: AppColor.primaryColor,
                         primaryButtonTxt: 'View',
                         ontapView: () {
+                          Map<String, String> timeData = (user.time
+                                  as Map<dynamic, dynamic>)
+                              .map((key, value) =>
+                                  MapEntry(key.toString(), value.toString()));
+
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (c) => ProviderDetails(
-                                profile: user.profile,
-                                name: "${user.firstName} ${user.lastName}",
-                                bio: user.bio,
-                                horseRate: user.hoursrate,
-                                experience: user.reference.experience,
-                                degree: user.education,
-                                dayButtons: dayButtons,
-                                timeData: timeData,
-                              ),
-                            ),
+                                builder: (c) => ProviderDetails(
+                                      profile: user.profile,
+                                      name:
+                                          "${user.firstName}} ${user.lastName}",
+                                      bio: user.bio,
+                                      horseRate: user.hoursrate,
+                                      experience: user.reference.experience,
+                                      degree: user.education,
+                                      dayButtons: dayButtons,
+                                      timeData: timeData,
+                                    )),
                           );
                         },
                         profile: user.profile,
