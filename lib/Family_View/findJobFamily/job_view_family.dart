88bbd58d3ab79856d643f0e2da7eca9763b1f984
Basit ali <basitalyshah51214@gmail.com>
2024-08-16 20:31:12 -0700

    remove unused code  from family job view

--- lib/Family_View/findJobFamily/job_view_family.dart
+++ lib/Family_View/findJobFamily/job_view_family.dart
@@ -7,10 +7,8 @@ import 'package:nanny_fairy/Repository/family_home_ui_repository.dart';
 import 'package:nanny_fairy/res/components/widgets/family_job_enums.dart';
 import 'package:nanny_fairy/res/components/widgets/family_job_search_bar.dart';
 import 'package:nanny_fairy/res/components/widgets/vertical_spacing.dart';
-import 'package:nanny_fairy/utils/routes/routes_name.dart';
 import 'package:nanny_fairy/view/filter/widgets/family_job_filter_popup.dart';
 import 'package:provider/provider.dart';
-import '../../FamilyController/family_home_controller.dart';
 import '../../res/components/colors.dart';
 
 class JobViewFamily extends StatefulWidget {
@@ -23,7 +21,6 @@ class JobViewFamily extends StatefulWidget {
 class _JobViewFamilyState extends State<JobViewFamily> {
   @override
   Widget build(BuildContext context) {
-    final familyhomeController = Provider.of<FamilyHomeController>(context);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: AppColor.secondaryBgColor,
@@ -99,80 +96,6 @@ class _JobViewFamilyState extends State<JobViewFamily> {
                 return selectedWidget;
               },
             ),
-            // SizedBox(
-            //   height: MediaQuery.of(context).size.height,
-            //   child: Padding(
-            //     padding: const EdgeInsets.only(left: 16, right: 16),
-            //     child: FutureBuilder(
-            //       future: familyhomeController.getPopularJobs(),
-            //       builder: (context, snapshot) {
-            //         if (snapshot.connectionState == ConnectionState.waiting) {
-            //           return const Center(child: CircularProgressIndicator());
-            //         } else if (snapshot.hasError) {
-            //           return Center(child: Text('Error: ${snapshot.error}'));
-            //         } else if (snapshot.hasData) {
-            //           Map<dynamic, dynamic> bookings =
-            //               snapshot.data as Map<dynamic, dynamic>;
-            //           List<Widget> bookingWidgets = [];
-
-            //           bookings.forEach((key, value) {
-            //             if (value['Availability'] is Map) {
-            //               Map<String, dynamic> availabilityMap =
-            //                   Map<String, dynamic>.from(value['Availability']);
-            //               Set<String> daysSet = {};
-            //               availabilityMap.forEach((timeOfDay, daysMap) {
-            //                 if (daysMap is Map) {
-            //                   daysMap.forEach((day, isAvailable) {
-            //                     if (isAvailable && !daysSet.contains(day)) {
-            //                       daysSet
-            //                           .add(day.substring(0, 1).toUpperCase());
-            //                     }
-            //                   });
-            //                 }
-            //               });
-
-            //               List<Widget> dayButtons =
-            //                   daysSet.map((dayAbbreviation) {
-            //                 return Padding(
-            //                   padding: const EdgeInsets.all(4.0),
-            //                   child: DayButtonFamily(day: dayAbbreviation),
-            //                 );
-            //               }).toList();
-
-            //               bookingWidgets.add(
-            //                 BookingCartWidgetHome(
-            //                   primaryButtonColor: AppColor.primaryColor,
-            //                   primaryButtonTxt: 'View',
-            //                   ontapView: () {},
-            //                   profile: value['profile'],
-            //                   name:
-            //                       "${value['firstName']} ${value['lastName']}",
-            //                   degree: value['education'],
-            //                   skill: '',
-            //                   hoursRate: value['hoursrate'],
-            //                   dayButtons: dayButtons,
-            //                 ),
-            //               );
-            //             } else {
-            //               bookingWidgets.add(
-            //                 const Center(child: Text('Invalid data format')),
-            //               );
-            //             }
-            //           });
-
-            //           return SingleChildScrollView(
-            //             scrollDirection: Axis.vertical,
-            //             child: Column(
-            //               children: bookingWidgets,
-            //             ),
-            //           );
-            //         } else {
-            //           return const Center(child: Text('No data available'));
-            //         }
-            //       },
-            //     ),
-            //   ),
-            // ),
           ],
         ),
       ),
@@ -189,6 +112,7 @@ class DayButtonFamily extends StatefulWidget {
   });
 
   @override
+  // ignore: library_private_types_in_public_api
   _DayButtonFamilyState createState() => _DayButtonFamilyState();
 }
 
