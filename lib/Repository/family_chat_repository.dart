4480cedaacfdf33b547d38545268a11cdc5ccbcc
Basit ali <basitalyshah51214@gmail.com>
2024-08-17 01:26:22 -0700

    changes to fix the issues of the save multiple messages

--- lib/Repository/family_chat_repository.dart
+++ lib/Repository/family_chat_repository.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:uuid/uuid.dart';
 
 class FamilyChatRepository {
   final DatabaseReference firestore = FirebaseDatabase.instance.ref();
@@ -14,35 +15,38 @@ class FamilyChatRepository {
     String senderProfilePic,
     String receiverProfilePic,
   ) async {
-    int timestamp = timeSent.millisecondsSinceEpoch;
+    try {
+      int timestamp = timeSent.millisecondsSinceEpoch;
 
-    Map<String, dynamic> receiverChatContact = {
-      "name": senderName,
-      "profilePic": senderProfilePic,
-      "timeSent": timestamp,
-      "lastMessage": text,
-      "familyId": auth.currentUser!.uid,
-    };
-    await firestore
-        .child("Providers")
-        .child(providerId)
-        .child("chats")
-        .child(auth.currentUser!.uid)
-        .set(receiverChatContact);
+      Map<String, dynamic> receiverChatContact = {
+        "name": senderName,
+        "profilePic": senderProfilePic,
+        "timeSent": timestamp,
+        "lastMessage": text,
+        "familyId": auth.currentUser!.uid,
+      };
+      await firestore
+          .child("Providers")
+          .child(providerId)
+          .child("chats")
+          .child(auth.currentUser!.uid)
+          .set(receiverChatContact);
 
-    Map<String, dynamic> senderChatContact = {
-      "name": receiverName,
-      "receiverProfilePic": receiverProfilePic,
-      "timeSent": timestamp,
-      "lastMessage": text,
-      "providerId": providerId,
-    };
-    await firestore
-        .child("Family")
-        .child(auth.currentUser!.uid)
-        .child("chats")
-        .child(providerId)
-        .set(senderChatContact);
+      Map<String, dynamic> senderChatContact = {
+        "name": receiverName,
+        "receiverProfilePic": receiverProfilePic,
+        "timeSent": timestamp,
+        "lastMessage": text,
+        "providerId": providerId,
+      };
+      await firestore
+          .child("Family")
+          .child(auth.currentUser!.uid)
+          .child("chats")
+          .child(providerId)
+          .set(senderChatContact);
+      saveMessageToDatabase(text, timestamp, providerId);
+    } catch (e) {}
   }
 
   Stream<DatabaseEvent> getFamilyChatStream() {
@@ -51,5 +55,48 @@ class FamilyChatRepository {
         firestore.child("Family").child(userId).child("chats");
 
     return familyChatRef.orderByChild("timeSent").onValue;
+  }
+
+  Future<void> saveMessageToDatabase(
+    String text,
+    int timeSent,
+    String providerId,
+  ) async {
+    try {
+      var uuid = const Uuid().v1();
+      print("Generated UUID: $uuid");
+
+      await firestore
+          .child("Providers")
+          .child(providerId)
+          .child("chats")
+          .child(auth.currentUser!.uid)
+          .child("messages")
+          .child(uuid)
+          .set({
+        "message": text,
+        "timeSent": timeSent,
+        'senderId': providerId,
+        "messageId": uuid,
+      });
+
+      await firestore
+          .child("Family")
+          .child(auth.currentUser!.uid)
+          .child("chats")
+          .child(providerId)
+          .child("messages")
+          .child(uuid)
+          .set({
+        "message": text,
+        "timeSent": timeSent,
+        'senderId': providerId,
+        "messageId": uuid,
+      });
+
+      print("Message saved successfully.");
+    } catch (e) {
+      print("Failed to save message: $e");
+    }
   }
 }
